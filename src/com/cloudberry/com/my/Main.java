869c029491e5346743cb6ef669ab49e6b035ac9f
Wu Chia Chong <chiachong.wu@lintramax.com>
2018-06-13 14:23:23 +0800

    Init

---
+++ src/com/cloudberry/com/my/Main.java
@@ -0,0 +1,19 @@
+package com.cloudberry.com.my;
+
+public class Main {
+	public static void main(String args[]) {
+		int i = 3;
+        i++;
+        // prints 4
+        System.out.println(i);
+        ++i;			   
+        // prints 5
+        System.out.println(i);
+        // prints 6
+        System.out.println(++i);
+        // prints 6
+        System.out.println(i++);
+        // prints 7
+        System.out.println(i);
+	}
+}
